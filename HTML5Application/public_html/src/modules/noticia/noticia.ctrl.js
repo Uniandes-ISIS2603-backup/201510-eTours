ca8d62818c686c9e25bf61e41fd868916bd27387
Laura Cortès <>
2015-03-04 12:18:47 -0500

    Modificaciones

---
+++ HTML5Application/public_html/src/modules/noticia/noticia.ctrl.js
@@ -0,0 +1,11 @@
+(function () {
+	var app = angular.module('noticiaModule');
+
+	app.controller('noticiaCtrl', ['$scope', 'CRUDUtils', 'noticia.context', function ($scope, CRUDUtils, context) {
+			this.url = context;
+			CRUDUtils.extendCtrl(this, $scope);
+			this.fetchRecords();
+		}]);
+            
+       
+})();
