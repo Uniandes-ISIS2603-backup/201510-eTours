130e7cfa0ae7bb4dc3618bc5e09ec191104702a7
Laura Cortès <>
2015-03-05 09:18:42 -0500

    Merge origin/master
    Conflicts:
    	HTML5Application/public_html/src/modules/hotel/hotel.tpl.html
    
    Cambios 5 en NOTICIAS BOTONES

--- HTML5Application/public_html/src/modules/evento/evento.mod.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var eventoModule = angular.module('eventoModule', ['CrudModule', 'MockModule']);
-
-    eventoModule.constant('evento.context', 'eventos');
-
-    eventoModule.config(['evento.context', 'MockModule.urlsProvider', function (context, urlsProvider) {
-            urlsProvider.registerUrl(context);
-        }]);
-})();
--- HTML5Application/public_html/src/modules/noticia/noticia.ctrl.js
+++ HTML5Application/public_html/src/modules/noticia/noticia.ctrl.js
@@ -5,7 +5,5 @@
 			this.url = context;
 			CRUDUtils.extendCtrl(this, $scope);
 			this.fetchRecords();
-                        
-                        
 		}]);
 })();
