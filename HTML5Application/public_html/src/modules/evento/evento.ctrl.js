8adf14163f6594f7ed389b73fb8734aa4d07ee7c
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-03-07 00:20:44 -0500

    Arreglado Angular

--- HTML5Application/public_html/src/modules/evento/evento.ctrl.js
+++ HTML5Application/public_html/src/modules/evento/evento.ctrl.js
@@ -5,19 +5,5 @@
 			this.url = context;
 			CRUDUtils.extendCtrl(this, $scope);
 			this.fetchRecords();
-                        this.showMost = false;
-                        this.mostPopulated = function (){
-                            this.showMost = true;
-                            $scope.mostPopulated = "Ninguno";
-                            var actual = 0;
-                            angular.forEach($scope.records, function (record){
-                                if(record.poblacion > actual){
-                                    actual = record.poblacion;
-                                    $scope.mostPopulated = record.name; 
-                                }
-                            });
-//                            
-                        };
-                        
 		}]);
 })();
