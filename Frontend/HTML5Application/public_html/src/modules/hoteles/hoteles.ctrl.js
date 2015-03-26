cd607569e64a217d1c574302c6ab16d0eeeb72e7
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-03-26 01:30:21 -0500

    Restaurantes listos

--- Frontend/HTML5Application/public_html/src/modules/hoteles/hoteles.ctrl.js
+++ Frontend/HTML5Application/public_html/src/modules/hoteles/hoteles.ctrl.js
@@ -5,8 +5,25 @@
         CRUDUtils.extendCtrl(this, $scope);
         this.fetchRecords();
         
+        this.imageFeatured = function() {
+            $scope.featured = 1;
+        }
+        
+        this.changeImageFeatured = function() {
+            if($scope.featured == 1) {
+                $scope.featured = 2;
+            } else if($scope.featured == 2) {
+                $scope.featured = 3;
+            } else if($scope.featured == 3) {
+                $scope.featured = 0;
+            } else {
+                $scope.featured = 1;
+            }
+        }
+        
         this.initTimeline = function(){
             $scope.inverted='timeline-inverted';
         }
+        
 		}]);
 })();
