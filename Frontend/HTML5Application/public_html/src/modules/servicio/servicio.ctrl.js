cb225e1d9aebead0f9aee6c303685f805c373a04
bdrangel10 <bd.rangel10@uniandes.edu.co>
2015-03-26 12:27:46 -0500

    Persistencia de servicios
    Modificaciones en el controlador del servicio

--- Frontend/HTML5Application/public_html/src/modules/servicio/servicio.ctrl.js
+++ Frontend/HTML5Application/public_html/src/modules/servicio/servicio.ctrl.js
@@ -1,8 +1,20 @@
 (function () {
 	var app = angular.module('servicioModule');
-	app.controller('servicioCtrl', ['$scope', 'CRUDUtils', 'servicio.context', function ($scope, CRUDUtils, context) {
+	app.controller('servicioCtrl', ['$scope', 'CRUDUtils', 'servicio.context', function ($scope, CRUDUtils, context, $localStorage) {
 			this.url = context;
 			CRUDUtils.extendCtrl(this, $scope);
 			this.fetchRecords();
+        
+            this.fetchRecords = function () 
+            {
+					var self = this;
+					this.api.getList().then(function (data) {
+						$scope.records = data;
+						$scope.currentRecord = {};
+						self.editMode = false;
+					}); 
+                localStorage.setItem('servicios-persistencia', JSON.stringify($scope.records));
+                
+            };
 		}]);
 })();
--- Frontend/HTML5Application/public_html/src/shared/crud/crud.svc.js
+++ Frontend/HTML5Application/public_html/src/shared/crud/crud.svc.js
@@ -1,7 +1,7 @@
 (function () {
 	var crud = angular.module('CrudModule');
 
-	crud.factory('CRUDUtils', ['Restangular', function (RestAngular, $localStorage) {
+	crud.factory('CRUDUtils', ['Restangular', function (RestAngular) {
 			function CRUD($scope) {
 				this.api = RestAngular.all(this.url);
 				$scope.currentRecord = {};
@@ -15,14 +15,15 @@
 						$scope.currentRecord = {};
 						self.editMode = false;
 					});
-                                        localStorage.setItem('servicios', JSON.stringify($scope.records));
+                    
+                                       
 				};
                               
                                 
 				this.createRecord = function () {
 					this.editMode = true;
 					$scope.currentRecord = {};
-                                        localStorage.setItem('servicios', JSON.stringify($scope.records));
+                    
 				};
 				this.saveRecord = function () {
 					var self = this;
@@ -35,20 +36,21 @@
 							self.fetchRecords();
 						});
 					}
+                    
+                    
+                    
                                         
-                                        localStorage.setItem('servicios', JSON.stringify($scope.records));
+                    
 				};
 				this.deleteRecord = function (record) {
 					var self = this;
 					record.remove().then(function () {
 						self.fetchRecords();
 					});
-                                        localStorage.setItem('servicios', JSON.stringify($scope.records));
 				};
 				this.editRecord = function (record) {
 					$scope.currentRecord = RestAngular.copy(record);
 					this.editMode = true;
-                                        localStorage.setItem('servicios', JSON.stringify($scope.records));
 				};
 			}
 			;
