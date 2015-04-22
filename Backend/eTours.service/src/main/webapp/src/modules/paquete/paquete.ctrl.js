d5d89801670506bb4e19f25f16fb3526403bdbb6
bdrangel10 <bd.rangel10@uniandes.edu.co>
2015-04-22 10:25:10 -0500

    Integración B-F

--- Backend/eTours.service/src/main/webapp/src/modules/paquete/paquete.ctrl.js
+++ Backend/eTours.service/src/main/webapp/src/modules/paquete/paquete.ctrl.js
@@ -8,7 +8,17 @@
             
 
             
-            
+         function skipUrl(entity_url)
+       { 
+           var fullUrl = baseUrl + '/' + entity_url;
+        var url_regexp = new RegExp(fullUrl + '/([0-9]+)');
+       $httpBackend.whenGET(fullUrl).passThrough();
+       $httpBackend.whenGET(url_regexp).passThrough(); 
+       $httpBackend.whenPOST(fullUrl).passThrough();
+       $httpBackend.whenPUT(url_regexp).passThrough(); 
+       $httpBackend.whenDELETE(url_regexp).passThrough();
+       //hasta aquí
+}   
 
 
              this.armarListaScope = function($scope){
--- Backend/eTours.service/src/main/webapp/src/modules/restaurantes/restaurantes.ctrl.js
+++ Backend/eTours.service/src/main/webapp/src/modules/restaurantes/restaurantes.ctrl.js
@@ -13,6 +13,19 @@
             $scope.featured = 1;
         }
         
+        function skipUrl(entity_url)
+       { 
+           var fullUrl = baseUrl + '/' + entity_url;
+        var url_regexp = new RegExp(fullUrl + '/([0-9]+)');
+       $httpBackend.whenGET(fullUrl).passThrough();
+       $httpBackend.whenGET(url_regexp).passThrough(); 
+       $httpBackend.whenPOST(fullUrl).passThrough();
+       $httpBackend.whenPUT(url_regexp).passThrough(); 
+       $httpBackend.whenDELETE(url_regexp).passThrough();
+       //hasta aquí
+}   
+        
+        
         this.changeImageFeatured = function() {
             if($scope.featured == 1) {
                 $scope.featured = 2;
