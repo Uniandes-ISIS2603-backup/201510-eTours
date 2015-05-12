b152fc839fc1c97fb0cb0c78e35875eafac4f141
bdrangel10 <bd.rangel10@uniandes.edu.co>
2015-05-11 22:05:09 -0500

    Eliminado COM

--- Backend/eTours.logic/src/main/java/co/edu/uniandes/csw/eTours/SolicitudRetiro/logic/ejb/SolicitudRetiroLogic.java
+++ Backend/eTours.logic/src/main/java/co/edu/uniandes/csw/eTours/SolicitudRetiro/logic/ejb/SolicitudRetiroLogic.java
@@ -43,6 +43,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.List;
 
 @Default
 @Stateless
